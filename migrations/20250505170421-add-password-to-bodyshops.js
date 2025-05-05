0e8665c1fbfcc1fd6e815de21ab9cc7df36cfb35
MariusJohn <marius.ciuclea@gmail.com>
2025-05-05 19:43:09 +0100

    bodyshop database migrations created

--- migrations/20250505170421-add-password-to-bodyshops.js
+++ migrations/20250505170421-add-password-to-bodyshops.js
@@ -1,12 +1,13 @@
- 'use strict';
+'use strict';
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Bodyshops', 'password', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: ''  // Required to avoid existing rows breaking
-    });
+    // await queryInterface.addColumn('Bodyshops', 'password', {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    //   defaultValue: ''  // Required to avoid existing rows breaking
+    // });
+    console.log('Skipping add-password migration'); // Optional: Add a log message
   },
 
   async down(queryInterface, Sequelize) {
