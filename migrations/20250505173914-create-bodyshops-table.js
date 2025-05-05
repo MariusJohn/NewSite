76761c943ca3ea518f4c7923e71a8c07e07b39b4
MariusJohn <marius.ciuclea@gmail.com>
2025-05-05 20:36:15 +0100

    database bodyshops migrated , registering step sorted

--- migrations/20250505173914-create-bodyshops-table.js
+++ migrations/20250505173914-create-bodyshops-table.js
@@ -14,6 +14,14 @@ module.exports = {
         allowNull: false,
         unique: true
       },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
       password: {
         type: Sequelize.STRING,
         allowNull: false
