9ac061af36cca500247d9e30434e8d7c90d9eac9
MariusJohn <marius.ciuclea@gmail.com>
2025-05-05 19:43:09 +0100

    bodyshop database migrations created

--- migrations/20250505172632-add-area-to-bodyshops.js
+++ migrations/20250505172632-add-area-to-bodyshops.js
@@ -2,11 +2,12 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Bodyshops', 'area', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: '' // Or your desired default value
-    });
+    // await queryInterface.addColumn('Bodyshops', 'area', {
+    //   type: Sequelize.STRING,
+    //   allowNull: false,
+    //   defaultValue: ''
+    // });
+    console.log('Skipping add-area migration'); // Optional: Add a log message
   },
 
   async down(queryInterface, Sequelize) {
