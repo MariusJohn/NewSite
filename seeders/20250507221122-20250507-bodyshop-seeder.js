d1d48bc61d7b8060db5504fb568e03519be25d52
MariusJohn <marius.ciuclea@gmail.com>
2025-05-07 23:59:51 +0100

    admin-dash finished bodyshop login page wip

---
+++ seeders/20250507221122-20250507-bodyshop-seeder.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const bodyshops = [
+      {
+        name: 'Quick Fix Motors',
+        email: 'quickfix@gmail.com',
+        password: await bcrypt.hash('QuickFix123!', 10),
+        area: 'NW1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Fast Lane Repairs',
+        email: 'fastlane@gmail.com',
+        password: await bcrypt.hash('FastLane123!', 10),
+        area: 'E2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'City Auto Repair',
+        email: 'cityauto@gmail.com',
+        password: await bcrypt.hash('CityAuto123!', 10),
+        area: 'W1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Metro Bodyworks',
+        email: 'metrobody@gmail.com',
+        password: await bcrypt.hash('MetroBody123!', 10),
+        area: 'SE1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Elite Car Bodyshop',
+        email: 'elitecar@gmail.com',
+        password: await bcrypt.hash('EliteCar123!', 10),
+        area: 'SW1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'The Dent Doctor',
+        email: 'dentdoctor@gmail.com',
+        password: await bcrypt.hash('DentDoc123!', 10),
+        area: 'N1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ];
+
+    await queryInterface.bulkInsert('Bodyshops', bodyshops);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Bodyshops', null, {});
+  }
+};
