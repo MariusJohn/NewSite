feb77d9e99e12c0739881d27a49b6d21ba4de86e
MariusJohn <marius.ciuclea@gmail.com>
2025-05-08 20:11:15 +0100

    bodyshop login/register/reset  style complete

--- public/js/navbar-toggle.js
+++ public/js/navbar-toggle.js
@@ -8,3 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+function togglePasswordVisibility(element) {
+    const input = element.previousElementSibling;
+    if (input.type === "password") {
+        input.type = "text";
+        element.textContent = "🙈";
+    } else {
+        input.type = "password";
+        element.textContent = "👁️";
+    }
+};
